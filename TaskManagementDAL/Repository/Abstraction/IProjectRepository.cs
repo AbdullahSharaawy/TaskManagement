9123dbc01009e6646c5946e24f5cfcef91483d50
AbdullahSharaawy <abdallahsharawy200@gmail.com>
2026-07-21 18:14:15 +0300

    added  interfaces of repository layer

---
+++ TaskManagementDAL/Repository/Abstraction/IProjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementDAL.Entities;
+
+namespace TaskManagementDAL.Repository.Abstraction
+{
+    public interface IProjectRepository
+    {
+        Task<project> GetByIdAsync(int id);
+        Task<IEnumerable<project>> GetAllAsync();
+        Task<project> AddAsync(project project);
+        Task<project> UpdateAsync(project project);
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
---
+++ TaskManagementDAL/Repository/Abstraction/ITaskRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementDAL.Repository.Abstraction
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<Task>> GetAllTasks();
+        Task<IEnumerable<Task>> GetTasksByProjectId(int projectId);
+        Task<Task> GetTaskById(int id);
+        Task<Task> AddAsync(Task task);
+        Task<Task> UpdateAsync(Task task);
+        Task<bool> DeleteAsync(int id);
+
+    }
+}
