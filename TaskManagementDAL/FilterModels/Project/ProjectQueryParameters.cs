8062f90b461a5e72a4148c425b2b9b8cb39c23cf
AbdullahSharaawy <abdallahsharawy200@gmail.com>
2026-07-23 01:19:51 +0300

    added the models of pagination, search, filter, and sort to DAL

---
+++ TaskManagementDAL/FilterModels/Project/ProjectQueryParameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementDAL.FilterModels.Project
+{
+    public class ProjectQueryParameters
+    {
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; } = 20;
+    }
+}
---
+++ TaskManagementDAL/FilterModels/Task/TaskQueryParameters.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementDAL.Enums.Filter;
+using TaskManagementDAL.Enums;
+
+namespace TaskManagementDAL.FilterModels.Task
+{
+    public class TaskQueryParameters
+    {
+        public Status? Status { get; set; }
+        public Priority? Priority { get; set; }
+        public DateTime? DueDateFrom { get; set; }
+        public DateTime? DueDateTo { get; set; }
+        public string? Search { get; set; }
+        public SortBy SortBy { get; set; } = SortBy.created_at;
+        public SortDirection SortDirection { get; set; } = SortDirection.desc;
+        public int Page { get; set; } = 1;
+        public int Limit { get; set; } = 20;
+    }
+}
